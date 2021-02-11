13b05737d5e08e56c4257865371a909665e5cfc0
saradbb <76662395+saradbb@users.noreply.github.com>
2021-02-11 05:51:19 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const app = express()
 app.use(bodyParser.urlencoded({extended:true}))
 //const url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=be40bef2471781380f2771d0d44b13e4"
 
-const API_KEY = "205c4482b265843af48b9c5feebbf058"
+const API_KEY = "205c4482b265843af48b9c5feebbf058"     // ;p If you want valuable API key: Bad luck
 app.get('/', (req,res) =>{
+  //Send the form
 res.sendFile(__dirname + '/index.html')
 })
 
@@ -29,16 +30,17 @@ res.write('<head><meta charset="utf-8"></head>');
 })
 */
 app.post('/',(req,res) => {
-cityName = req.body.city
-const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + API_KEY
-https.get(url,(response) =>
-response.on("data", (data) =>{
-  const weatherData = JSON.parse(data)
+  //Post request after the form is submitted
+cityName = req.body.city   //Parse the city name from the form
+const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + API_KEY   //URL to make an API call 
+https.get(url,(response) =>       //Get request
+response.on("data", (data) =>{                      //on the response
+  const weatherData = JSON.parse(data)              //Convert JSON string to JSON Object
   const temp = weatherData.main.temp
   const weatherDescription = weatherData.weather[0].description
   const icon = weatherData.weather[0].icon
-  const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-  res.write('<head><meta charset="utf-8"></head>');
+  const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"    //To get the image icon representing current weather
+  res.write('<head><meta charset="utf-8"></head>');                        //to ensure image is displayed
     res.write("<h1>The temperature in" + cityName + " is "+ temp + " degree celcius.</h1>");
     res.write("<h1> The weather is "+weatherDescription+"</h1>");
     res.write("<img src = " + imgUrl +">");
